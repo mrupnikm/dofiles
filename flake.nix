7569a042959a0090d308fddb87da6617a35d41d1
Matic Rupnik <matic.rupnik@marand.si>
2024-07-28 18:29:44 +0200

    v2

--- flake.nix
+++ flake.nix
@@ -16,8 +16,11 @@
     homeConfigurations = {
         "mrupnik" = home-manager.lib.homeManagerConfiguration {
 
-            pkgs = import nixpkgs { system = "x86_64-linux"; };
-
+            #pkgs = import nixpkgs { system = "x86_64-linux"; };
+            pkgs = import nixpkgs { 
+               system = "x86_64-linux"; 
+               config.allowUnfree = true;
+            };
             modules = [ ./home.nix ];
         };
     };
